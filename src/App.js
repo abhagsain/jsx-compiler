ea8402dbbc85c8fc032bf7a27c7232c72cb2f19e
Anurag Bhagsain <abhagsain@gmail.com>
2019-08-22 00:00:17 +0530

    everything work fine

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css"
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {error: "", code_input: ``,compiled_code: ""}
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  onCodeUpdate = ({target: {value}}) => {
+    try {
+      
+      let code = value;
+      this.setState({
+        compiled_code: window.Babel
+        .transform(code, { presets: ['es2015', 'react']})
+        .code,
+        error: ''
+      })
+    } catch (err) {
+      this.setState({error: err.message})
+    }
+  }
+  render() {
+    const {error,code_input,compiled_code} = this.state;
+    return (
+      <div >
+      <div className="header">
+      <h2>Live JSX Compiler Using Babel</h2>
+      <br></br>
+      <header className="error">{error}</header>
+      </div>
+      <div className="container">
+        <div className="code_area">
+          <textarea name="text_area"  placeholder="/* Add your JSX Code here */" rows="30" cols="90" onChange={this.onCodeUpdate} defaultValue={code_input}/>
+        </div>
+        <div className="pre_area">
+          <pre>{compiled_code}</pre>
+        </div>
+      </div>
+      <div>
+      
+      <div className="copy">
+      <h2>Copy Paste this code</h2>
+      <br></br>
+      <pre style={{background: "#444", color: "white", padding: "20px"}}>
+      {`  
+      function Header(props){
+        return <header>{props.title}</header>
+      }
+      function App(){
+        return <div style={{background: "blue"}}><h1>Babel is going to compile this </h1> <Header title="I'm a prop" /></div>
+      }`}
+      </pre>
+
+      </div>
+      </div>
+      </div>
+    
+    )
+  }
 }
-
-export default App;
